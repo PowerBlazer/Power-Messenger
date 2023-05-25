3da2168933832f7929648a25a1101b525f83e293
Айнур Якупов <104465016+PowerBlazer@users.noreply.github.com>
2023-05-25 19:43:04 +0300

    Update RefreshTokenValidation.cs

--- PowerMessenger.Application/Features/AuthorizationFeature/RefreshToken/RefreshTokenValidation.cs
+++ PowerMessenger.Application/Features/AuthorizationFeature/RefreshToken/RefreshTokenValidation.cs
@@ -6,6 +6,10 @@ public class RefreshTokenValidation: AbstractValidator<RefreshTokenCommand>
 {
     public RefreshTokenValidation()
     {
-        
+        RuleFor(p => p.AccessToken)
+            .NotEmpty().WithMessage("Поле не может быть пустым");
+
+        RuleFor(p => p.RefreshToken)
+            .NotEmpty().WithMessage("Поле не может быть пустым");
     }
 }
